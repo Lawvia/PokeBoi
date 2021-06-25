aa5328635fb8d832f89405e469b6142d565f6372
lawvia <rsndvdsn@gmail.com>
2021-06-25 10:16:01 +0700

    fetching dynamic on SW

--- public/worker.js
+++ public/worker.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'pokeBoi';
+var CACHE_NAME = 'pokeBoi-v2';
 var urlsToCache = [
   '/',
   '/index.html'
@@ -25,8 +25,28 @@ self.addEventListener('fetch', event => {
         // Cache hit - return response
         if (response) {
           return response;
+        }else{
+          return fetch(event.request)
+            .then(function(res) {
+              return caches.open('dynamic')
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                })
+            })
+            .catch(function (err){
+              console.log("dari fetch ",err)
+              return caches.open('dynamic')
+                .then(function (cache){
+                  console.log("obaboaob")
+                  // if (event.request.url.indexOf('https://graphql-pokeapi.vercel.app/api/graphql')){
+                    console.log("ini errror get fetch")
+                    return cache.match('https://graphql-pokeapi.vercel.app/api/graphql');
+                  // }
+                })
+            });
         }
-        return fetch(event.request);
+        // return fetch(event.request);
       }
     )
   );
@@ -34,7 +54,7 @@ self.addEventListener('fetch', event => {
 
 // Update a service worker
 self.addEventListener('activate', event => {
-  var cacheWhitelist = ['pokeBoi'];
+  var cacheWhitelist = [CACHE_NAME,'dynamic'];
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
